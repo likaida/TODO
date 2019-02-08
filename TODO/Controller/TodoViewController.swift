767f8ed31f0fdb37ddf1283898c2dc71c8110d5e
Ace <1264626829@qq.com>
2019-02-08 13:05:31 +0800

    MyTodoProject

---
+++ TODO/Controller/TodoViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  TODO
+//
+//  Created by Ace on 2019/2/8.
+//  Copyright © 2019年 Ace. All rights reserved.
+//
+
+import UIKit
+
+class TodoViewController: UITableViewController {
+    let itemArray = ["购买水杯","吃药","修改代码"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell",for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+             tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+
+
+}
+
